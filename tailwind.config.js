5cb7e543aa44d51139423090f2ece2e0e3eb35b5
mbukerepo <mbuke404@gmail.com>
2022-10-09 13:24:05 +0200

    applied changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,19 +10,35 @@ module.exports = {
         lightBlue: "#232F3E",
       },
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
+        sans: ["Roboto", "sans-serif"],
       },
       textColor: {
         primary: "#1D2130",
       },
+      borderColor: {
+        success: "#E0E4FC",
+      },
       backgroundColor: {
         primary: "#1D2130",
+        secondary: "#2b2e3c",
+        success: "#E0E4FC",
+        danger: "#d798e126",
+        lightGray: "#F0F2FE",
       },
       fontWeight: {
         bolder: 700,
+        normal: 400,
       },
       borderColor: {
         primary: "#1D2130",
+      },
+      fontSize: {
+        xs: ["16px", "150%"],
+        sm: ["18px", "150%"],
+        md: ["24px", "140%"],
+        lg: ["36px", "120%"],
+        xl: ["48px", "120%"],
+        "2xl": ["56px", "110%"],
       },
     },
   },
